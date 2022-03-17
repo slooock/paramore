e4598bff475df7d3ebf522e4d6276b6df12d7ff8
Kayque Avelar <slooock@Kayques-iMac.local>
2022-03-17 08:45:48 -0300

    paramore Billie

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Paramore é bom pacarai</p>
+        <p>Paramore é melhor que Billie Eilish </p>
         <a
           className="App-link"
           href="https://reactjs.org"
