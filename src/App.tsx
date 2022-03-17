443a3745b3ea2c2201a236ae002398db749c692f
Kayque Avelar <slooock@Kayques-iMac.local>
2022-03-17 09:19:45 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Paramore é melhor que Billie Eilish </p>
+        <p>
+          Paramore é melhor que Billie Eilish. Tolkien é melhor que Paramore
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
